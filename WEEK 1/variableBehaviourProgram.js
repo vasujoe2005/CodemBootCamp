7c7c6f187797dd84ca24b696ae7b137c27e709eb
vasujoe2005 <vasujoe1985@gmail.com>
2026-02-10 23:37:42 +0530

    10-02-2026 commit

---
+++ WEEK 1/variableBehaviourProgram.js
@@ -0,0 +1,18 @@
+//var variable
+var a=10;
+console.log("After declaration(a=10):",a);
+a=20;
+console.log("After reassignment(a=20):",a);
+var a=30; 
+console.log("After redeclaration(var a=30):",a);
+//let variable 
+let b=40;
+console.log("After declaration(b=40):",b);
+b=50; 
+console.log("After reassignment(b=50):",b);
+//let b=60;
+//const varaible
+const c=70;
+console.log("After declaration(c=70):",c);
+//c=80;
+//const c=90;
